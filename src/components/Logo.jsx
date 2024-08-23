c6c138b4322843d6e6d0201ac9e48bf0cfef000b
ArjitMishra2502 <arjitmishra2002@gmail.com>
2024-08-23 14:16:15 +0530

    Add files via upload

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div class="logo-img"><img src="https://miro.medium.com/v2/resize:fit:1358/1*8Hznl83pUZLuQUYwblnzPQ.png" alt="" /></div>
+  )
+}
+
+export default Logo
